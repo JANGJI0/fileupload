f797b700ec39bcfef1a359878b4586c57f9d9c53
GD <GD@DESKTOP-GGRP423>
2025-04-25 17:06:26 +0900

    2025.04.25 파일업로드 구현 (마지막 css...잠시만 안녕...)
    전체적으로 수정, css 다듬었다.
    
    잠시만 css는 떠나보내고 자격증, 백엔드에 집중해야겠다.

--- src/main/java/com/example/fileupload/entity/BoardMapping.java
+++ src/main/java/com/example/fileupload/entity/BoardMapping.java
@@ -3,4 +3,6 @@ package com.example.fileupload.entity;
 public interface BoardMapping {
 	int getBno();
 	String getTitle();
+	
+	
 }
--- src/main/java/com/example/fileupload/entity/Boardfile.java
+++ src/main/java/com/example/fileupload/entity/Boardfile.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data
@@ -25,7 +26,16 @@ public class Boardfile {
 	private String fext;
 	@Column(name = "fsize")
 	private long fsize;
-	
+	@Transient // DB 컬럼으로 저장되지 않음
+	private boolean isThumbnail;
+
+	public boolean isThumbnail() {
+	    return isThumbnail;
+	}
+
+	public void setThumbnail(boolean isThumbnail) {
+	    this.isThumbnail = isThumbnail;
+	}
 	
 	// 자식에서 부모로 단방향 관계설정 O
 	// 불필요한 연관 관계는 조인등으로 인해 시스템 부하를 가져옴 -> 조인이 필요한 비지니스로직이 있을 때만 연관 관계를 설정
